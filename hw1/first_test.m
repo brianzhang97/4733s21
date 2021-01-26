ae0506cab26f9993296ce1b8b7d9d7f61b3666c0
Jan Mandel <jan.mandel@gmail.com>
2021-01-26 16:49:35 -0700

    adding file hw1/first_test.m

---
+++ hw1/first_test.m
@@ -0,0 +1,10 @@
+function z=first_test(x,y)
+% z=first_test(x,y)
+% This is just to set up and test the homework workflow.
+% Add a line of code below that will compute matrix z as matrix x times matrix y.
+
+
+% To get the credit, this function needs to work and return the correct value.
+% Do not change the name of the file or the function line in any way, it would not run.
+% You may want to write another function to test this function.
+end
