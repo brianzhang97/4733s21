c2ed98e627371cc4874d91da8fd18678ecf72333
brianzhang97 <64829551+brianzhang97@users.noreply.github.com>
2021-01-28 23:18:17 -0500

    Updated first_test.m
    Define a function first_test that computes matrix z as matrix x times matrix y.

--- hw1/first_test.m
+++ hw1/first_test.m
@@ -1,9 +1,9 @@
-function z=first_test(x,y)
+function z = first_test(x,y)
 % z=first_test(x,y)
 % This is just to set up and test the homework workflow.
 % Add a line of code below that will compute matrix z as matrix x times matrix y.
 
-
+ z = x*y; 
 % To get the credit, this function needs to work and return the correct value.
 % Do not change the name of the file or the function line in any way, it would not run.
 % You may want to write another function to test this function.
