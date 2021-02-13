4c9a074e1c003779dc74b3b5f5768b625cd16e58
Jan Mandel <jan.mandel@gmail.com>
2021-02-12 23:05:11 -0700

    adding hw4 files

--- hw4/trapezoidal_test.m
+++ hw4/trapezoidal_test.m
@@ -8,11 +8,11 @@ dt = 1/n;     % the time step length
 a = -1;        % the coefficient
 [u,t]=trapezoidal(a,0,dt,n,1);
 err(1)=u(n+1)-exp(a)
-figure(1),plot(t,u),hold on,fplot(@(t)exp(a*t),[0,1]),hold off  % you can uncomment this to plot a picture for a visual check
+%figure(1),plot(t,u),hold on,fplot(@(t)exp(a*t),[0,1]),hold off  % you can uncomment this to plot a picture for a visual check
 
 a = -25;        % new coefficient, same dt and n
 [u,t]=trapezoidal(a,0,dt,n,1);
 err(2)=u(n+1)-exp(a)
-figure(2),plot(t,u),hold on,fplot(@(t)exp(a*t),[0,1]),hold off % you can uncomment this to plot a picture for a visual check
+%figure(2),plot(t,u),hold on,fplot(@(t)exp(a*t),[0,1]),hold off % you can uncomment this to plot a picture for a visual check
 
 end
